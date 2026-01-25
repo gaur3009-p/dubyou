e05863a369be92017749d8a6baa819b82df63442
gaur3009-p <gaur3009@gmail.com>
2026-01-26 00:18:32 +0530

    Add voice prompts for enrollment

---
+++ services/voice_enrollment/prompts.py
@@ -0,0 +1,7 @@
+VOICE_PROMPTS = [
+    "Hello, my name is Aditya and this is my voice enrollment.",
+    "I enjoy building technology that helps people communicate.",
+    "Artificial intelligence will change how humans interact.",
+    "Today is a beautiful day and I am feeling confident.",
+    "Please remember my voice clearly and naturally."
+]
